ce52509d10879af8c0eeb74fe1abc52fba4a3612
Warg4rd <thomas.couderc@limayrac.fr>
2022-02-01 17:25:32 +0100

    [ADD][UPDATE] index and add css files

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css" />
+    <link rel="stylesheet" href="css/fond.css" />
+    <title>Maison des Ligues</title>
+</head>
+<body>
+<header role="header">
+    <nav class="menu" role="navigation">
+        <div class="inner">
+            <div class="m-left">
+                <a href="#" class="m-link">Accueil</a>
+                <a href="#" class="m-link">FAQ</a>
+            </div>
+            <div class="m-right">
+                <a href="#" class="m-link">Se connecter</a>
+                <a href="#" class="m-link">S'inscrire</a>
+            </div>
+    </nav>
+    <nav P class="blocktext"></nav>
+</header>
+</body>
+</html>
