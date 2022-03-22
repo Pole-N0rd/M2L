ea3777929802f5efe54658ecbbd055ed5b2bd501
Simoniaque <simon.auriac@limayrac.fr>
2022-03-22 15:10:41 +0100

    [add] page de la foire au question ou l'on peut ajouter supprimer ou modifier une question

---
+++ FAQ.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="FAQcss.css">
+    <title>FAQ</title>
+</head>
+
+<body>
+    <h1>Foire au question</h1>
+    <form>
+        <div>
+            <input type="search" id="Recherche" name="Recherche" placeholder="Rechercher une question le site…" size="100">
+            <button>Rechercher</button>
+            <button>Ajouter une question</button>
+        </div>
+    </form>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <ul class="timeline">
+                    <li>
+                        <a target="_blank">Emilio Dingo</a></br>
+                        <a class="Date">21 March, 2020</a></br></br>
+                        <a class="Question"> Nam pellentesque felis vitae justo accumsan ?</a></br></br>
+                        <a class="Reponse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                        <p> <input type="submit" name="submit" value="Modifier">&nbsp;<input type="reset" value="Supprimer"></p>
+                    </li>
+                    <li>
+                        <a target="_blank">papan la fripouille</a></br>
+                        <a class="Date">21 March, 2020</a></br></br>
+                        <a class="Question"> Nam pellentesque felis vitae justo accumsan ?</a></br></br>
+                        <a class="Reponse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                        <p> <input type="submit" name="submit" value="Modifier">&nbsp;<input type="reset" value="Supprimer"></p>
+                    </li>
+                    <li>
+                        <a target="_blank">JeSuis</a></br>
+                        <a class="Date">21 March, 2020</a></br></br>
+                        <a class="Question"> Nam pellentesque felis vitae justo accumsan ?</a></br></br>
+                        <a class="Reponse">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula....</p>
+                        <p> <input type="submit" name="submit" value="Modifier">&nbsp;<input type="reset" value="Supprimer"></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
